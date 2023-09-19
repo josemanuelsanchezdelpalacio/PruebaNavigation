f27809e670f3a0cc02beec9d7c97f53b169e056b
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-09-19 14:15:56 +0200

    Prueba app kotlin Boton y mensaje

--- AppPrueba/app/src/main/java/com/example/appprueba/MainActivity.kt
+++ AppPrueba/app/src/main/java/com/example/appprueba/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.appprueba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSaludo: Button
+    private lateinit var txtSaludo: TextView
+    private lateinit var edtIntroduceNombre: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSaludo = findViewById(R.id.btnSaludo)
+        txtSaludo = findViewById(R.id.txtSaludo)
+        edtIntroduceNombre = findViewById(R.id.edtIntroduceNombre)
+
+        btnSaludo.setOnClickListener{
+            txtSaludo.text = "Hola " + edtIntroduceNombre.getText().toString()
+        }
     }
 }
