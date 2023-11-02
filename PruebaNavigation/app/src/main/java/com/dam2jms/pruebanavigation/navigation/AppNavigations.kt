203edcc7bdb592057c6151f08695027b55a153cc
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-11-02 09:05:08 +0100

    prueba navigation

---
+++ PruebaNavigation/app/src/main/java/com/dam2jms/pruebanavigation/navigation/AppNavigations.kt
@@ -0,0 +1,8 @@
+package com.dam2jms.pruebanavigation.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun appNavigation(){
+
+}
--- PruebaNavigation/app/src/main/java/com/dam2jms/pruebanavigation/navigation/AppScreen.kt
+++
@@ -1,6 +0,0 @@
-package com.dam2jms.pruebanavigation.navigation
-
-sealed class AppScreen (val route: String){
-    object FirstScreen: AppScreen(route = "firs_screen")
-    object SecondScreen: AppScreen(route = "second_screen")
-}
